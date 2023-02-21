dfddd2442648a2a8c8ecdf62e42755a21e78c58e
Ghani-25 <54942476+Ghani-25@users.noreply.github.com>
2023-02-21 11:18:28 +0100

    Update model.py

--- model.py
+++ model.py
@@ -6,7 +6,7 @@ import torch
 from sentence_transformers import SentenceTransformer, util
 
 model = SentenceTransformer("Ghani-25/LF_enrich_sim", device='cpu')
-url = "https://drive.google.com/uc?export=download&id=1-GEVcdInQ1QIaPsOYKJDoUcxmVpnhmFM"
+url = "https://huggingface.co/datasets/Ghani-25/Embeddings_full_Enrichments/resolve/main/Embeddings_full"
 output = "Embeddings_full"
 gdown.download(url, output, quiet=False)
 with open("./Embeddings_full", "rb") as fp:
